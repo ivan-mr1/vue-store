bdabb547d499fe64fe70832c665b3b4bd53a1941
ivan-mr1 <ivanitit3535@gmail.com>
2026-01-18 15:57:13 +0200

    feat: add components: Card and CardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('add')
+}
+
+const onClickFavorite = () => {
+  alert('add to favorites')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="mens shoes Nice"
+      :price="88"
+      image-url="assets/img/sneakers/sneakers-1.jpg"
+      :is-added="false"
+      :is-favorite="false"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite"
+    />
+  </div>
+</template>
